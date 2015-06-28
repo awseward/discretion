6c867167f5ad88d5acb728c775fd3ef6a628f209
Andrew Seward <awswrd@gmail.com>
2015-06-28 12:15:46 -0700

    Add rake task to install hooks locally

--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,21 @@
 require 'rake/testtask'
 
+task :default => [:test]
+
 Rake::TestTask.new do |t|
   t.test_files = FileList['**/*_test.rb']
   t.verbose = true
 end
 
-task :default => [:test]
+namespace :hooks do
+  desc 'Copies all hooks in `hooks/` to `.git-hooks/`'
+  task :install do
+    git_hooks = Dir.chdir(".git-hooks"){ Dir.pwd }
+
+    Dir.chdir "hooks" do
+      Dir.glob "**/*.rb" do |path|
+        cp_r path, "#{git_hooks}/#{path}"
+      end
+    end
+  end
+end
