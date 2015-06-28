f0b44fb8964a23a41f0defc1f6efeeaff7a3af12
Andrew Seward <awswrd@gmail.com>
2015-06-28 11:25:54 -0700

    Add some actual tests

--- spec/pre_push/print_diff_urls_test.rb
+++ spec/pre_push/print_diff_urls_test.rb
@@ -1,14 +1,25 @@
 require 'minitest/autorun'
 require 'overcommit'
 require 'overcommit/hook/pre_push/base'
+require 'uri'
 require './hooks/pre_push/print_diff_urls'
 
 class TestDiffUrls < Minitest::Test
   def setup
     @formatter = GitRemoteUrlFormatter.new
+    @ssh_url = "git@github.com:some_user/some-repo.git"
   end
 
-  def test_example
-    assert_equal 1, 0
+  def test_base_url_formatting
+    http_url = @formatter.format_base_url @ssh_url
+
+    assert http_url =~ URI::regexp
+  end
+
+  def test_compare_url_formatting
+    branch = "some_feature_branch"
+    compare_url = @formatter.compare_url @ssh_url, branch
+
+    assert compare_url =~ URI::regexp
   end
 end
