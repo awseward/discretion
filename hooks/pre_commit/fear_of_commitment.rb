7320ee2827d19ed3f8ac04a5bcb118067133e9f2
Andrew Seward <awswrd@gmail.com>
2015-06-29 07:13:19 -0700

    Implement FearOfCommitment

--- hooks/pre_commit/fear_of_commitment.rb
+++ hooks/pre_commit/fear_of_commitment.rb
@@ -7,7 +7,51 @@ module Overcommit::Hook::PreCommit
     end
 
     def run
-      [:warn, "TODO"]
+      checks = [
+        :too_many_files,
+        :too_many_lines
+      ]
+
+      if checks.any?{|c| send c }
+        messages = [
+          "Modified by this commit:",
+          "  Files: #{total_modified_files}",
+          "  Lines: #{total_modified_lines}",
+          "Prefer making smaller, more frequent commits",
+        ]
+
+        [:warn, messages.join("\n")]
+      else
+        :pass
+      end
+    end
+
+    private
+
+    def max_files_modified
+      config['max_files_modified'] || 20
+    end
+
+    def max_lines_modified
+      config['max_lines_modified'] || 200
+    end
+
+    def too_many_files
+      total_modified_files > max_files_modified
+    end
+
+    def too_many_lines
+      total_modified_lines > max_lines_modified
+    end
+
+    def total_modified_files
+      @context.modified_files.count
+    end
+
+    def total_modified_lines
+      @context.modified_files.map do |file|
+        modified_lines_in_file(file).size
+      end.reduce(:+)
     end
   end
 end
