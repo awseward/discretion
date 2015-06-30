75ec60ba73d824cf9a453f8eeb6cf010c0a6fe81
Andrew Seward <awswrd@gmail.com>
2015-06-29 19:29:37 -0700

    Oops I broke the tests

--- spec/pre_push/print_diff_urls_test.rb
+++ spec/pre_push/print_diff_urls_test.rb
@@ -6,19 +6,18 @@ require './hooks/pre_push/print_diff_urls'
 
 class TestDiffUrls < Minitest::Test
   def setup
-    @formatter = GitRemoteUrlFormatter.new
     @ssh_url = "git@github.com:awseward/overcommit-hooks.git"
   end
 
   def test_base_url_formatting
-    http_url = @formatter.format_base_url @ssh_url
+    http_url = GitUrlFormat.format_base_url @ssh_url
 
     assert http_url =~ URI::regexp
   end
 
   def test_compare_url_formatting
     branch = "some_feature_branch"
-    compare_url = @formatter.compare_url @ssh_url, branch
+    compare_url = GitUrlFormat.compare_url @ssh_url, branch
 
     assert compare_url =~ URI::regexp
   end
