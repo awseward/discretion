409a20ca0acaaba70325eedbaa028cd727b9699f
Andrew Seward <awswrd@gmail.com>
2015-07-09 15:32:15 -0700

    Add a single skipped test
    As a reminder/placeholder

---
+++ spec/pre_commit/fear_of_commitment_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'overcommit'
+require 'overcommit/hook/pre_commit/base'
+require './hooks/pre_commit/fear_of_commitment'
+
+class TestFearOfCommitment < Minitest::Test
+  def test_placeholder
+    skip "TODO"
+  end
+end
