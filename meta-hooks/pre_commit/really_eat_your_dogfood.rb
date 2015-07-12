0d6faf634079785c8a5b5715100cbd6229b792f2
Andrew Seward <awswrd@gmail.com>
2015-07-11 23:33:43 -0700

    Working for now

--- meta-hooks/pre_commit/really_eat_your_dogfood.rb
+++ meta-hooks/pre_commit/really_eat_your_dogfood.rb
@@ -1,4 +1,5 @@
 require 'overcommit'
+require 'yaml'
 
 module Overcommit::Hook::PreCommit
   class ReallyEatYourDogfood < Base
@@ -7,7 +8,44 @@ module Overcommit::Hook::PreCommit
     end
 
     def run
-      [:warn, "TODO"]
+      [:warn, 'TODO']
+    end
+
+    private
+    def get_config_hash
+      YAML.load_file '.overcommit.yml'
+    end
+
+    def filename_to_classname(filename)
+      no_ext = File.basename filename, File.extname(filename)
+
+      no_ext.split("_").collect(&:capitalize).join
+    end
+
+    def get_hook_type(path)
+      filename_to_classname File.dirname(path)
+    end
+
+    def base_enabled_hash(plugin)
+      {plugin => {"enabled" => true}}
+    end
+
+    def get_installed_plugins
+      paths = Dir['*hooks/**/*.rb']
+
+      paths.reduce({}) do |seed, curr|
+        type = get_hook_type curr
+        name = filename_to_classname curr
+
+        if seed.has_key?(type)
+          hooks = seed[type]
+          new_hash = {type => hooks.merge(base_enabled_hash(name))}
+          seed.merge new_hash
+        else
+          new_hash = {type => base_enabled_hash(name)}
+          seed.merge new_hash
+        end
+      end
     end
   end
 end
